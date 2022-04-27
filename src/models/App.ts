fec2a42e7b53865ce825d325157fea9cdb02f5ac
Michael Myers <michaelmyers@users.noreply.github.com>
2022-04-27 15:55:21 -0400

    fix: graphql-tag missing dependency (#335)

--- src/models/App.ts
+++ src/models/App.ts
@@ -1,11 +1,14 @@
 /*! Copyright (c) 2022, XAPP AI*/
+import { App as _GraphqlApp } from "../graphql/models";
+
+
+export type GraphqlApp = Omit<_GraphqlApp, "_id" | "cmsTokens" | "faqQuery">;
 
 export interface App {
     appId: string;
     name: string;
     organizationId: string;
     invocationName?: string;
-    //
     actionsOnGoogleId?: string;
 }
 
